861f2e4a803a3dc56ce8608f315d916a9c02a5c5
Zhouyu Qian <qzy@qzy.io>
2019-07-01 16:59:02 -0700

    Growable stack

--- green_threads.cpp
+++ green_threads.cpp
@@ -1,12 +1,17 @@
 #include <assert.h>
 #include <deque>
+#include <signal.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-#define STACK_SIZE (1048576)
+#define STACK_SIZE (8192)
+#define STACK_MAX_SIZE (1024 * STACK_SIZE)
+#define STACK_ALLOC_SIZE (2 * STACK_MAX_SIZE)
 #define DEFAULT_THREADS 4
 
 enum class ThreadState {
@@ -59,14 +64,27 @@ struct Thread {
     explicit Thread(bool is_system_thread)
       : stack(nullptr), state(ThreadState::Dead) {
         if (!is_system_thread) {
-            stack = malloc(STACK_SIZE);
-            if (!stack) {
-                fputs("Could not allocate memory for new thread.\n", stderr);
+            size_t guard_size = STACK_ALLOC_SIZE - STACK_SIZE;
+            stack = mmap(0, STACK_ALLOC_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON,
+                         -1, 0);
+            if (stack == MAP_FAILED) {
+                fputs(
+                  "Could not allocate virtual address space for new thread "
+                  "stack.\n",
+                  stderr);
+                exit(1);
+            }
+            if (mprotect((unsigned char*) stack + guard_size,
+                         STACK_ALLOC_SIZE - guard_size,
+                         PROT_READ | PROT_WRITE) == -1) {
+                fputs("Could not set protection on stack.\n", stderr);
                 exit(1);
             }
         }
     }
-    ~Thread() { free(stack); }
+    ~Thread() {
+        if (stack) munmap(stack, 2 * STACK_MAX_SIZE);
+    }
     Thread(Thread const&) = delete;
     Thread& operator=(Thread const&) = delete;
     Thread(Thread&& th) : stack(th.stack), ctx(th.ctx), state(th.state) {
@@ -74,7 +92,7 @@ struct Thread {
         th.state = ThreadState::Dead;
     }
     Thread& operator=(Thread&& th) {
-        free(stack);
+        if (stack) munmap(stack, 2 * STACK_MAX_SIZE);
         stack = th.stack;
         ctx = th.ctx;
         state = th.state;
@@ -137,6 +155,7 @@ private:
 
         if (threads.empty()) {
             // No other thread to yield to. Resume running the current thread.
+            assert(current_thread.state != ThreadState::Dead);
             return false;
         } else {
             // Yield to a new and different thread at the front of the queue.
@@ -159,21 +178,87 @@ private:
         // Place the address of exit_thread on top of the stack, so that if the
         // green thread function returns, it returns into that function.
         void (*guard)() = Runtime::exit_thread;
-        memcpy((unsigned char*) new_thread.stack + STACK_SIZE - 8, &guard, 8);
-        memcpy((unsigned char*) new_thread.stack + STACK_SIZE - 16, &func, 8);
+        memcpy((unsigned char*) new_thread.stack + STACK_ALLOC_SIZE - 8, &guard,
+               8);
+        memcpy((unsigned char*) new_thread.stack + STACK_ALLOC_SIZE - 16, &func,
+               8);
 
         // The rsp of a new thread points to the address of the function we want
         // to run, because the first real instruction after switching stack and
         // state is "ret".
         new_thread.ctx.rsp =
-          (uint64_t)((unsigned char*) new_thread.stack + STACK_SIZE - 16);
+          (uint64_t)((unsigned char*) new_thread.stack + STACK_ALLOC_SIZE - 16);
         new_thread.state = ThreadState::Ready;
         threads.emplace_back(std::move(new_thread));
     }
     [[noreturn]] void do_run() {
+        setup_growable_stack();
         while (do_yield())
             ;
         exit(0);
+    }
+
+    ptrdiff_t do_get_current_stack_usage() const {
+        unsigned char* rsp;
+        __asm__ volatile("movq %%rsp, %0" : "=r"(rsp));
+        if (current_thread.stack) {
+            return (unsigned char*) current_thread.stack + STACK_ALLOC_SIZE -
+                   rsp;
+        } else {
+            return -1;
+        }
+    }
+
+    // Grow a green thread stack in response to a segfault.
+    void grow_stack(void* fault_addr) const {
+        // Round down to page
+        fault_addr = (void*) ((uint64_t) fault_addr & ~0xfffull);
+
+        if (fault_addr >=
+              (unsigned char*) current_thread.stack + STACK_MAX_SIZE &&
+            fault_addr <
+              (unsigned char*) current_thread.stack + STACK_ALLOC_SIZE) {
+            if (mprotect(fault_addr,
+                         (unsigned char*) current_thread.stack +
+                           STACK_ALLOC_SIZE - (unsigned char*) fault_addr,
+                         PROT_READ | PROT_WRITE)) {
+                const char msg[] = "No more memory for thread stacks.\n";
+                write(2, msg, sizeof msg - 1);
+                _exit(5);
+            }
+        } else {
+            const char msg[] = "Unexpected segfault.\n";
+            write(2, msg, sizeof msg - 1);
+            _exit(2);
+        }
+    }
+
+    static void do_grow_stack(int sig, siginfo_t* info, void* ucontext) {
+        (void) sig;
+        (void) ucontext;
+        void* fault_addr = info->si_addr;
+        instance().grow_stack(fault_addr);
+    }
+
+    void setup_growable_stack() {
+        static char sigstack[81920];
+        stack_t st = {};
+        st.ss_sp = sigstack;
+        st.ss_size = sizeof sigstack;
+        if (sigaltstack(&st, NULL) == -1) {
+            perror("sigaltstack");
+            exit(1);
+        }
+
+        struct sigaction sa = {};
+        sa.sa_flags = SA_ONSTACK | SA_SIGINFO;
+        sa.sa_sigaction = do_grow_stack;
+
+        if (sigaction(SIGSEGV, &sa, NULL) == -1 ||
+            sigaction(SIGBUS, &sa, NULL) == -1) {
+            perror("sigaction");
+            exit(1);
+        }
     }
 
 public:
@@ -181,28 +266,50 @@ public:
     static void spawn(void (*func)()) { return instance().do_spawn(func); }
     static void run_forever() { return instance().do_run(); }
     static void exit_thread() { return instance().ret(); }
+    static ptrdiff_t get_current_stack_usage() {
+        return instance().do_get_current_stack_usage();
+    }
 };
 
-static void thread1() {
-    puts("thread 1 started");
+static void func1() {
+    puts("func 1 started");
     for (int i = 0; i < 10; ++i) {
-        printf("thread 1 counting %d\n", i);
+        printf("func 1 counting %d\n", i);
         Runtime::yield();
     }
 }
 
-static void thread2() {
-    puts("thread 2 started");
+static void func2() {
+    puts("func 2 started");
     for (int i = 0; i < 10; ++i) {
-        printf("thread 2 counting %d\n", i);
+        printf("func 2 counting %d\n", i);
         Runtime::yield();
+        printf("func 2 current stack usage: %ld\n",
+               Runtime::get_current_stack_usage());
     }
-    puts("thread 2 finished counting; calling thread1 again");
-    Runtime::spawn(thread1);
+    puts("func 2 finished counting; spawning func 1 again");
+    Runtime::spawn(func1);
+}
+
+static uint64_t lots_of_stack(uint64_t m, uint64_t n) {
+    printf("Current stack usage: %ld\n", Runtime::get_current_stack_usage());
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return lots_of_stack(m - 1, 1);
+    Runtime::yield();
+    auto rv = lots_of_stack(m - 1, lots_of_stack(m, n - 1));
+    printf("ackermann(%llu, %llu) = %llu\n", m, n, rv);
+    return rv;
+}
+
+static void do_use_lots_of_stack() {
+    printf("final result = %llu\n", lots_of_stack(4, 4));
 }
 
 int main() {
-    Runtime::spawn(thread1);
-    Runtime::spawn(thread2);
+    Runtime::spawn(func1);
+    Runtime::spawn(func2);
+    Runtime::spawn(do_use_lots_of_stack);
     Runtime::run_forever();
 }
